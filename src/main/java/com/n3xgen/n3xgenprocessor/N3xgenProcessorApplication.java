5e3def23e9432de445f607fc76ed38e719279954
EmmanuelArreola <emmanuel.arreola@cloudgensys.com>
2022-06-15 09:34:56 -0500

    Fixed issue on ProcessorNodeJsImpl that only shows errors, included new bindings parameters

--- src/main/java/com/n3xgen/n3xgenprocessor/N3xgenProcessorApplication.java
+++ src/main/java/com/n3xgen/n3xgenprocessor/N3xgenProcessorApplication.java
@@ -3,20 +3,19 @@ package com.n3xgen.n3xgenprocessor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.n3xgen.n3xgenprocessor.service.ProcessorNodeJs;
+import com.n3xgen.n3xgenprocessor.service.ProcessorNodeJsImpl;
 
 @SpringBootApplication
 public class N3xgenProcessorApplication {
-
+	private static final org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(N3xgenProcessorApplication.class);
 	public static void main(String[] args) {
+		log.info("Main method starting");
 		 SpringApplication.run(N3xgenProcessorApplication.class, args);
-//		String data = "{\n"
-//				+ "  example: [\n"
-//				+ "    {value: 4},\n"
-//				+ "    {value: 7},\n"
-//				+ "    {value: 13}\n"
-//				+ "  ]\n"
-//				+ "}";
-//		String expression = "$sum(example.value)";
+//		String data = "{\"state\":\"Karnataka\",\n"
+//				+ "\"code\":\"KA\",\n"
+//				+ "\"country\":\"India\"}";
+//		String expression = "{ 'age': state, 'name': code}";
 //		ProcessorNodeJs transformer = new ProcessorNodeJsImpl();
 //		transformer.nodeJsToJSONprocessor(data, expression);
 	}
--- src/main/java/com/n3xgen/n3xgenprocessor/service/ProcessorNodeJsImpl.java
+++ src/main/java/com/n3xgen/n3xgenprocessor/service/ProcessorNodeJsImpl.java
@@ -1,22 +1,30 @@
 package com.n3xgen.n3xgenprocessor.service;
 
 import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.PrintStream;
 
 import com.n3xgen.n3xgenprocessor.bean.ExceptionPath;
 
 public class ProcessorNodeJsImpl implements ProcessorNodeJs {
+	private static final org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(ProcessorNodeJsImpl.class);
 
 	@Override
 	public String nodeJsToJSONprocessor(String payload, String expression) {
-		StringBuffer outputPayload = new StringBuffer();
+		// StringBuffer outputPayload = new StringBuffer();
+		// Create a stream to hold the output
+		ByteArrayOutputStream finalData = new ByteArrayOutputStream();
+		log.info("Starting expression");
 		try {
 			File tempFile = File.createTempFile("node", ".js");
 			FileOutputStream fos = new FileOutputStream(tempFile);
+			log.info(tempFile.getAbsolutePath());
 			StringBuilder expressionBuilder = new StringBuilder("var jsonata = require(\"jsonata\");");
 			expressionBuilder.append(System.lineSeparator());
 			expressionBuilder.append("var data=" + payload + ";");
@@ -29,30 +37,56 @@ public class ProcessorNodeJsImpl implements ProcessorNodeJs {
 			fos.write(expressionBuilder.toString().getBytes());
 			fos.close();
 			fos.flush();
+			log.info("It creates the temp file");
+			log.info(tempFile.getAbsolutePath());
+			PrintStream ps = new PrintStream(finalData);
+			// IMPORTANT: Save the old System.out!
+			PrintStream old = System.out;
+			// Tell Java to use your special stream
+			System.setOut(ps);
+			// Execute temp fil
 			Process pb = Runtime.getRuntime().exec("node " + tempFile.getAbsolutePath());
-			if (null != pb.getErrorStream()) {
-				BufferedReader error = new BufferedReader(new InputStreamReader(pb.getErrorStream()));
-				StringBuffer errorMsg = new StringBuffer();
-				String str;
-				while ((str = error.readLine()) != null)
-					errorMsg.append(str);
-				error.close();
-				throw new Exception(errorMsg.toString());
+			// Saves and print final JSON
+			try (InputStream in = pb.getInputStream();) {
+				byte[] bytes = new byte[2048];
+				int len;
+				while ((len = in.read(bytes)) != -1) {
+					System.out.write(bytes, 0, len);
+				}
 			}
-			BufferedReader input = new BufferedReader(new InputStreamReader(pb.getInputStream()));
-			String line;
-			while ((line = input.readLine()) != null)
-				outputPayload.append(line);
-			input.close();
+			// Put things back
+			System.out.flush();
+			System.setOut(old);
+			// Show what happened
+			System.out.println("Here: " + finalData.toString());
+
+			log.info("state : " + pb.exitValue());
+			System.out.println("Process exited with: " + pb.waitFor());
+//			if (null != pb.getErrorStream()) {
+//				BufferedReader error = new BufferedReader(new InputStreamReader(pb.getErrorStream()));
+//				StringBuffer errorMsg = new StringBuffer();
+//				String str;
+//				while ((str = error.readLine()) != null)
+//					errorMsg.append(str);
+//				error.close();
+//				throw new Exception(errorMsg.toString());
+//			}
+//			BufferedReader input = new BufferedReader(new InputStreamReader(pb.getInputStream()));
+//			log.info("Final result: " + pb.getInputStream());
+//			String line;
+//			while ((line = input.readLine()) != null)
+//				outputPayload.append(line);
+//			input.close();
 			tempFile.deleteOnExit();
 		} catch (FileNotFoundException e) {
-			throw new ExceptionPath("File do not exist or is unreacheable: " + e.getMessage());
+			throw new ExceptionPath("File do not exist or is unreacheable: " + e.getMessage() + e.getStackTrace());
 		} catch (IOException e) {
 			throw new ExceptionPath("Input or Output excpetion thrown by: " + e.getMessage());
 		} catch (Exception e) {
 			e.getStackTrace();
-			throw new ExceptionPath("Error on: " );
+			e.printStackTrace();
+			// throw new ExceptionPath("Error on: " );
 		}
-		return outputPayload.toString();
+		return finalData.toString();
 	};
 }
