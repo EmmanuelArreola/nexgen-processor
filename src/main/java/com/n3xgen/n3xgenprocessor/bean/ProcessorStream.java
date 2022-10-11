e4cdeacc5295dc5a546f43e2ec713e4d232bf459
EmmanuelArreola <emmanuel.arreola@cloudgensys.com>
2022-10-11 16:58:09 -0500

    Added the simple way to retrieve payload and cast it to String

--- src/main/java/com/n3xgen/n3xgenprocessor/bean/ProcessorStream.java
+++ src/main/java/com/n3xgen/n3xgenprocessor/bean/ProcessorStream.java
@@ -1,12 +1,5 @@
 package com.n3xgen.n3xgenprocessor.bean;
 
-import java.io.BufferedReader;
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.ObjectOutputStream;
 import java.util.function.Function;
 
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
@@ -16,8 +9,6 @@ import org.springframework.messaging.Message;
 
 import com.n3xgen.n3xgenprocessor.service.ProcessorNodeJs;
 import com.n3xgen.n3xgenprocessor.service.ProcessorNodeJsImpl;
-
-
 
 @EnableConfigurationProperties(ProcessorProperties.class)
 @Configuration
@@ -32,46 +23,12 @@ public class ProcessorStream {
 
 	@Bean
 	public Function<Message<?>, String> processTransformation() {
-		return payload ->  {
+		return payload -> {
 			log.info("Starting Application");
-			log.info(payload.getHeaders().get("contentType").toString());
 
-
-			ByteArrayOutputStream baos = new ByteArrayOutputStream();
-
-			try {
-				ObjectOutputStream oos = new ObjectOutputStream(baos);
-				oos.writeObject(payload.getPayload());
-				oos.flush();
-				oos.close();
-			} catch (IOException InputOutputException) {
-				
-				throw new ExceptionPath("Input or Output excpetion thrown by: " + InputOutputException.getMessage() 
-				+ InputOutputException.getStackTrace());
-			}
-
-		    InputStream is = new ByteArrayInputStream(baos.toByteArray());
-			
-			BufferedReader  data = new BufferedReader(new InputStreamReader( is ));
-						
-			StringBuffer sb = new StringBuffer();
-			String inputLine = "";
-		    try {
-		    	data.readLine();
-				while ((inputLine = data.readLine()) != null) {
-					log.info(inputLine);
-				    sb.append(inputLine);
-				}
-			} catch (IOException InputOutputException) {
-				throw new ExceptionPath("Input or Output excpetion thrown by: " + InputOutputException.getMessage() 
-				+ InputOutputException.getStackTrace());
-			}
-
-			log.info(sb.toString());
-			
 			ProcessorNodeJs processor = new ProcessorNodeJsImpl();
-			return processor.nodeJsToJSONprocessor(sb.toString(), processorProperties.getExpression());
-
+			return processor.nodeJsToJSONprocessor(new String((byte[]) payload.getPayload()),
+					processorProperties.getExpression());
 
 		};
 	}
