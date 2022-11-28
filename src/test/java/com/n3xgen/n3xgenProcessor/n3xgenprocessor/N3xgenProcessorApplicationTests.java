158b9f2fcfbee2675a518145d83d9dcb24909a0e
charly2791 <114499193+charly2791@users.noreply.github.com>
2022-11-28 13:52:04 -0600

    Add Json Validator
    Add library json-validator-cg-lib-0.0.1

--- src/test/java/com/n3xgen/n3xgenProcessor/n3xgenprocessor/N3xgenProcessorApplicationTests.java
+++ src/test/java/com/n3xgen/n3xgenProcessor/n3xgenprocessor/N3xgenProcessorApplicationTests.java
@@ -1,13 +1,18 @@
 package com.n3xgen.n3xgenProcessor.n3xgenprocessor;
 
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.n3xgex.jsonSchemaValidator.JsonSchemaValidator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import com.n3xgen.n3xgenprocessor.service.ProcessorNodeJs;
 import com.n3xgen.n3xgenprocessor.service.ProcessorNodeJsImpl;
+
+import java.util.List;
 
 @SpringBootTest
 class N3xgenProcessorApplicationTests {
@@ -16,17 +21,26 @@ class N3xgenProcessorApplicationTests {
 	ProcessorNodeJs testingMain = new ProcessorNodeJsImpl();
 
 //	Sample data
-	String sampleData = " {\r\n" + "\"FirstName\": \"Emmanuel\",\r\n"
-			+ "\"SecondName\": \"Arreola\",\r\n" + "\"Age\": \"25 years\",\r\n"
-			+ "\"LastName\": \"Perez\"\r\n" + "}";
-
+	String sampleData = " {\r\n" +
+		                  "\"FirstName\": \"Emmanuel\",\r\n"+
+		                  "\"SecondName\": \"Arreola\",\r\n" +
+		                  "\"Age\": \"25 years\",\r\n" +
+		                  "\"LastName\": \"Perez\"\r\n" +
+		                   "}";
+    String  in_schema="{}";
 	@Test
 	@DisplayName("Testing main method")
 	void testNodeJsToJSONprocessor() {
 
+        JsonSchemaValidator wrapperFromLocalResources = new JsonSchemaValidator();
+
+        List<JsonNode> a =  wrapperFromLocalResources.jsonValidator(in_schema,sampleData);
+        System.out.println(a.size() == 0 ? "true" : a);
+
+
 		String dataSample = testingMain.nodeJsToJSONprocessor(sampleData,
 				"$substring(FirstName, 2, 3) & $lowercase(SecondName) & $replace(LastName, \"Perez\", \"Cortes\") & $substringBefore(Age, \" years\")");
-		log.info(dataSample);
+	//	log.info(dataSample);
 		assertEquals("\"manarreolaCortes25\"", dataSample.trim());
 	}
 
